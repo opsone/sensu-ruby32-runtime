79fda2bfe1f1a8d849c2c62cd97c7d567cd2447c
Kevyn Lebouille <kevyn.lebouille@opsone.net>
2023-06-13 22:23:13 +0200

    Update build_platforms script

--- build_scripts/build_platforms.sh
+++ build_scripts/build_platforms.sh
@@ -4,20 +4,19 @@ mkdir -p dist
 mkdir -p assets
 
 # Debian platform
-platform="debian" ../build_platform.sh
-retval=$?
-if [[ retval -ne 0 ]]; then
-  exit $retval
-fi
-
 platform="debian10" ../build_platform.sh
 retval=$?
 if [[ retval -ne 0 ]]; then
   exit $retval
 fi
 
-
 platform="debian11" ../build_platform.sh
+retval=$?
+if [[ retval -ne 0 ]]; then
+  exit $retval
+fi
+
+platform="debian12" ../build_platform.sh
 retval=$?
 if [[ retval -ne 0 ]]; then
   exit $retval
